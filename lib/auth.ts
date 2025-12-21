0ebc18521299f8476693794718ce99e85406deb3
Swarnim Tripathi <swarnim.tr@gmail.com>
2025-12-21 23:59:52 +0530

    feat: enable dangerous email account linking for Google and GitHub providers

--- lib/auth.ts
+++ lib/auth.ts
@@ -16,6 +16,7 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+            allowDangerousEmailAccountLinking: true,
             authorization: {
                 params: {
                     scope: "openid email profile https://www.googleapis.com/auth/calendar.events.readonly",
@@ -27,6 +28,7 @@ export const authOptions: NextAuthOptions = {
         GitHubProvider({
             clientId: process.env.GITHUB_ID || "",
             clientSecret: process.env.GITHUB_SECRET || "",
+            allowDangerousEmailAccountLinking: true,
         }),
         CredentialsProvider({
             name: "Credentials",
